559d49a4d3336d5eb040497bbc486a2e76ac7cec
MaksSenchugov <144223100+MaksSenchugov@users.noreply.github.com>
2023-10-29 15:17:54 +0300

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,16 +1,16 @@
 const item1 = document.getElementsByTagName('span')[3]
 console.log(item1.innerHTML)
-item1.innerHTML = 'СЕНЧУГОВ'
+item1.innerHTML = 'SENCHUGOV'
 console.log(item1.innerHTML)
 
 const item2 = document.getElementsByTagName('span')[4]
 console.log(item2.innerHTML)
-item2.innerHTML = 'МАКСИМ'
+item2.innerHTML = 'MAKSIM'
 console.log(item2.innerHTML)
 
 const item3 = document.getElementsByTagName('span')[5]
 console.log(item3.innerHTML)
-item3.innerHTML = 'ИГОРЕВИЧ'
+item3.innerHTML = 'IGOREVICH'
 console.log(item3.innerHTML)
 
 const item4 = document.getElementsByTagName('span')[7]
