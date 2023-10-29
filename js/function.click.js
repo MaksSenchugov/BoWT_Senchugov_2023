d79bb1dade2ea761f334948a06108422fc8e70ce
MaksSenchugov <144223100+MaksSenchugov@users.noreply.github.com>
2023-10-29 15:39:55 +0300

    Create function.click.js

---
+++ js/function.click.js
@@ -0,0 +1,25 @@
+const node_for_click = document.getElementById("for_click")
+
+function find_edit(){
+const item1 = document.getElementsByTagName('span')[3]
+console.log(item1.innerHTML)
+item1.innerHTML = 'СЕНЧУГОВ'
+console.log(item1.innerHTML)
+
+const item2 = document.getElementsByTagName('span')[4]
+console.log(item2.innerHTML)
+item2.innerHTML = 'МАКСИМ'
+console.log(item2.innerHTML)
+
+const item3 = document.getElementsByTagName('span')[5]
+console.log(item3.innerHTML)
+item3.innerHTML = 'ИГОРЕВИЧ'
+console.log(item3.innerHTML)
+
+const item4 = document.getElementsByTagName('span')[7]
+console.log(item4.innerHTML)
+item4.innerHTML = '21.07.2004'
+console.log(item4.innerHTML)
+}
+
+node_for_click.addEventListener("click",find_edit)
